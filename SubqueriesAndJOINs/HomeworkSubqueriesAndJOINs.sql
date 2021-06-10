0f89897a06c969d0d9c197212be413e1988c78d1
Polina Pavlova <poli.paskaleva@gmail.com>
2021-06-10 21:53:39 +0300

    problem 15 added

--- SubqueriesAndJOINs/HomeworkSubqueriesAndJOINs.sql
+++ SubqueriesAndJOINs/HomeworkSubqueriesAndJOINs.sql
@@ -163,14 +163,30 @@ ORDER BY c.country_name
 LIMIT 5;
 
 #15
-SELECT max(select count(currency_code) from countries as c
-where c1.continent_code = c.continent_code
-GROUP BY continent_code, currency_code) as max_  from countries as c1
-group by continent_code;
-
-select count(currency_code) from countries as c
-
-GROUP BY currency_code;
+SELECT 
+    d1.continent_code, d1.currency_code, d1.currency_usage
+FROM
+    (SELECT 
+        `c`.`continent_code`,
+            `c`.`currency_code`,
+            COUNT(`c`.`currency_code`) AS `currency_usage`
+    FROM
+        countries AS c
+    GROUP BY c.currency_code , c.continent_code
+    HAVING currency_usage > 1) AS d1
+        LEFT JOIN
+    (SELECT 
+        `c`.`continent_code`,
+            `c`.`currency_code`,
+            COUNT(`c`.`currency_code`) AS `currency_usage`
+    FROM
+        countries AS c
+    GROUP BY c.currency_code , c.continent_code
+    HAVING currency_usage > 1) AS d2 ON d1.continent_code = d2.continent_code
+        AND d2.currency_usage > d1.currency_usage
+WHERE
+    d2.currency_usage IS NULL
+ORDER BY d1.continent_code , d1.currency_code;
 
 #16
 
