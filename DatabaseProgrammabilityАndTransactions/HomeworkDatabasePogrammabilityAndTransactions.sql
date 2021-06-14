dbe67a2f1355c5140bb147f097cea1abd1871a75
Polina Pavlova <poli.paskaleva@gmail.com>
2021-06-14 22:27:50 +0300

    some homework added

--- DatabaseProgrammabilityАndTransactions/HomeworkDatabasePogrammabilityAndTransactions.sql
+++ DatabaseProgrammabilityАndTransactions/HomeworkDatabasePogrammabilityAndTransactions.sql
@@ -54,7 +54,6 @@ END $$
 CALL usp_get_employees_from_town('Sofia');
 
 #5
-delimiter $$
 CREATE FUNCTION ufn_get_salary_level(employee_salary DECIMAL(10,2)) 
 RETURNS VARCHAR(50) 
 DETERMINISTIC
@@ -67,54 +66,53 @@ BEGIN
     END;
     RETURN salary_level;
     
-    END $$
-    
-    SELECT  salary,  ufn_get_salary_level(salary) from employees;
+    END;
     
 #6
+CREATE FUNCTION ufn_get_salary_level(employee_salary DECIMAL(10,2)) 
+RETURNS VARCHAR(50) 
+DETERMINISTIC
+BEGIN
+	DECLARE salary_level VARCHAR(50);
+	SET salary_level := case
+    when employee_salary < 30000 THEN 'Low'
+    when employee_salary BETWEEN 30000 AND 50000 THEN 'Average'
+    else 'High'
+    END;
+    RETURN salary_level;
+    
+    END;
+    
+    
 
-DELIMITER $$
+
+
 CREATE PROCEDURE usp_get_employees_by_salary_level(salary_level VARCHAR(50))
 BEGIN 
 SELECT e.first_name, e.last_name FROM employees as e
 WHERE ufn_get_salary_level(e.salary) = salary_level
 ORDER BY first_name DESC, last_name DESC;
-END$$
+END;
 
 
  CALL usp_get_employees_by_salary_level('high');
+ 
+ #7
+	
     
+    #8
     
-	#7
-delimiter $$ 
-create function ufn_is_word_comprised(set_of_letters varchar(50), word varchar(50))  
-RETURNS BOOLEAN
-DETERMINISTIC
-BEGIN
-DECLARE result BOOLEAN;
-SET result := word REGEXP(CONCAT('(?i)^[', set_of_letters, ']+$'));
-RETURN result;
-END$$
-
-SELECT ufn_is_word_comprised('oistmiahf', 'Sofia');
-
-
-#8
-
-DELIMITER $$
-CREATE PROCEDURE usp_get_holders_full_name () 
+    CREATE PROCEDURE usp_get_holders_full_name () 
 BEGIN
   SELECT concat_ws(' ', first_name, last_name) as full_name
   FROM account_holders
   ORDER BY full_name, id;
   
-END $$
+END;
 
 CALL usp_get_holders_full_name ();
 
 #9
-
-DELIMITER $$
 CREATE PROCEDURE usp_get_holders_with_balance_higher_than(number_parameter DECIMAL(10,2))
 BEGIN
 SELECT ah.first_name, ah.last_name  FROM account_holders as ah
@@ -126,14 +124,21 @@ ON a.account_holder_id = ah.id
  ) > number_parameter
  GROUP BY ah.id
 ORDER BY ah.id;
- END$$
- 
- 
+ END;
  
  CALL usp_get_holders_with_balance_higher_than(7000);
  
-#10
-DELIMITER $$
+ #10
+ CREATE FUNCTION ufn_calculate_future_value(sum DECIMAL(10,4), yearly_interest_rate DECIMAL(10,4), number_of_years INT)
+RETURNS DECIMAL(10,4)
+DETERMINISTIC
+BEGIN
+    DECLARE result DECIMAL(10,4);
+    SET result := sum * (pow((yearly_interest_rate  + 1),  number_of_years));
+	RETURN result;
+END;
+
+#11
 CREATE FUNCTION ufn_calculate_future_value(sum DECIMAL(10,4), yearly_interest_rate DECIMAL(10,4), number_of_years INT)
 RETURNS DECIMAL(10,4)
 DETERMINISTIC
@@ -141,10 +146,9 @@ BEGIN
     DECLARE result DECIMAL(10,4);
     SET result := sum * (pow((yearly_interest_rate  + 1),  number_of_years));
 	RETURN result;
-END $$
+END;
 
-#11
-DELIMITER $$
+
 CREATE PROCEDURE usp_calculate_future_value_for_account(account_id INT, interest DECIMAL(10,4))
 BEGIN
    SELECT a.id, ah.first_name, ah.last_name, a.balance,
@@ -152,70 +156,42 @@ BEGIN
  FROM accounts as a
  JOIN account_holders as ah ON a.account_holder_id = ah.id
  where a.id = account_id;
- END $$
+ END;
  
 CALL usp_calculate_future_value_for_account(1, 0.1);
 
 #12
-DELIMITER $$
 CREATE PROCEDURE usp_deposit_money(account_id INT, money_amount DECIMAL(10,4))
 BEGIN 
    UPDATE  accounts as a
-   
+   JOIN `account_holders` as ah ON a.`account_holder_id` = ah.id
    SET a.balance = if(money_amount>0, ROUND(a.balance + money_amount,4), a.balance)
    WHERE a.id = account_id;
-   END$$
+   END;
    
-CALL usp_deposit_money (1, 10);
-select * from accounts
-WHERE id = 1;
-
-#13
-
-DELIMITER $$
-CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(20,4))
+   #13
+   CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(20,4))
 BEGIN 
    UPDATE  accounts as a
+   JOIN `account_holders` as ah ON a.`account_holder_id` = ah.id
    SET a.balance = if((money_amount>0 and a.balance >= money_amount), ROUND(a.balance - money_amount,4), a.balance)
    WHERE a.id = account_id;
-   END$$
-
- 
-CALL usp_withdraw_money (1, 10);
-select * from accounts
-WHERE id = 1;
-
-#14
-
-
-DELIMITER $$
-CREATE PROCEDURE usp_transfer_money(from_account_id INT, to_account_id INT, amount DECIMAL(20,4))
+   END;
+   
+   #14
+   CREATE PROCEDURE usp_transfer_money(from_account_id INT, to_account_id INT, amount DECIMAL(20,4))
 BEGIN
-	
-	IF ((SELECT count(id) FROM accounts WHERE id = from_account_id) = 1
-    AND (SELECT count(id) FROM accounts WHERE id = to_account_id) = 1
-    AND (SELECT balance from accounts WHERE id = from_account_id) >= amount
-    AND amount>=0)
-    THEN
 	START TRANSACTION;
-	UPDATE accounts AS a 
-SET 
-    a.balance = ROUND(a.balance - amount, 4)
-WHERE
-    a.id = from_account_id;
-UPDATE accounts AS a 
-SET 
-    a.balance = ROUND(a.balance + amount, 4)
-WHERE
-    a.id = to_account_id;
-    ELSE ROLLBACK;
+	IF(((SELECT a.id FROM accounts as a WHERE a.id like from_account_id)<>1)
+    or ((SELECT a.id FROM accounts as a WHERE a.id like to_account_id)<>1)
+    or (SELECT a.balance from accounts as a WHERE a.id = from_account_id) 
+    or (amount <0) ) THEN
+	ROLLBACK;
+	ELSE
+		call usp_deposit_money(to_account_id, amount);
+        call usp_withdraw_money(from_account_id, amount);
 		END IF; 
-END $$
-
-call usp_transfer_money(1, 2, 10);
-select * from accounts 
-where id in (1,2)
-order by id;
+END;
 
 #15
 CREATE table `logs`(
@@ -223,6 +199,7 @@ log_id INT PRIMARY KEY AUTO_INCREMENT,
 account_id INT, 
 old_sum  DECIMAL(20,4), 
 new_sum DECIMAL(20,4));
+
 CREATE TRIGGER account_logs
 AFTER UPDATE
 ON accounts 
@@ -233,6 +210,7 @@ BEGIN
 END;
 
 #16
+
 CREATE table `logs`(
 log_id INT PRIMARY KEY AUTO_INCREMENT, 
 account_id INT, 
